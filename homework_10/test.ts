03e86936991fc17446df10b86f9da79778b47228
TonyZhang <zhangdshr@gmail.com>
2019-04-15 01:43:46 -0500

    homework_10

---
+++ homework_10/test.ts
@@ -0,0 +1,13 @@
+
+class University { 
+    name = 'MUM'; 
+    dept= 'Computer Science'
+}  
+
+function graduation(params:University) {
+        return class {
+        name = params.name;
+        dept = params.dept
+        year = 2019
+    }
+}
